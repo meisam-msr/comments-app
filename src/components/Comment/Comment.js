a0cea1cc956ded7daaca008090807f1116068cdd
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-09 15:53:44 +0330

    get data from api

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,9 +1,9 @@
 import "./comment.css";
-const Comment = () => {
+const Comment = ({ name, email }) => {
   return (
     <div className="comment">
-      <p>name</p>
-      <p>email</p>
+      <p>name : {name}</p>
+      <p>email : {email}</p>
     </div>
   );
 };
