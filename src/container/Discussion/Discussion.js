b29cb9031d617d9cb64d0b11094d49495a6d7afe
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-09 15:54:09 +0330

    get data from api with axios

--- src/container/Discussion/Discussion.js
+++ src/container/Discussion/Discussion.js
@@ -2,14 +2,47 @@ import Comment from "../../components/Comment/Comment";
 import FullComment from "../../components/FullComment/FullComment";
 import NewComment from "../../components/NewComment/NewComment";
 import "./discussion.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Discussion = () => {
+  const [comments, setComments] = useState(null);
+
+  useEffect(() => {
+    // axios
+    //   .get("https://jsonplaceholder.typicode.com/comments")
+    //   .then((response) => {
+    //     // console.log(response.data);
+    //     setComments(response.data.slice(0, 4));
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+
+    const getComments = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://jsonplaceholder.typicode.com/comments"
+        );
+        setComments(data.slice(0, 4));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getComments();
+  }, []);
+
   return (
     <main>
       <section>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments ? (
+          comments.map((c) => (
+            <Comment key={c.id} name={c.name} email={c.email} />
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
       </section>
       <section>
         <FullComment />
