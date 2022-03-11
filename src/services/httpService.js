53046c6526d7b0be5ff9764329645d023cbf367e
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-12 00:43:35 +0330

    add services

---
+++ src/services/httpService.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:3001";
+
+axios.interceptors.request.use(
+  (request) => {
+    console.log(request);
+    return request;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject();
+  }
+);
+axios.interceptors.response.use(
+  (response) => {
+    console.log(response);
+    return response;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject();
+  }
+);
+
+const http = {
+  get: axios.get,
+  post: axios.post,
+  put: axios.put,
+  delete: axios.delete,
+};
+
+export default http;
