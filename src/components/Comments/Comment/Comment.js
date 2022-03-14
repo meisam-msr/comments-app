4ac62b0f0947b6c568623f1b10af29bccbced878
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-14 23:47:28 +0330

    create comment cmp

---
+++ src/components/Comments/Comment/Comment.js
@@ -0,0 +1,11 @@
+import "./comment.css";
+const Comment = ({ name, email, onClick }) => {
+  return (
+    <div className="comment" onClick={onClick}>
+      <p>name : {name}</p>
+      <p>email : {email}</p>
+    </div>
+  );
+};
+
+export default Comment;
