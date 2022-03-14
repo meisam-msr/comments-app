3f8e9a27be3590b8109c9ce7f163df0990389f34
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-14 23:43:59 +0330

    create routes file

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import FullComment from "./pages/FullComment/FullComment";
+import HomePage from "./pages/HomePage";
+import NewComment from "./pages/NewComment/NewComment";
+import NotFound from "./pages/NotFound";
+
+const routes = [
+  { path: "/comment/:id", component: FullComment },
+  { path: "/new-comment", component: NewComment },
+  { path: "/", component: HomePage, exact: true },
+  { component: NotFound },
+];
+
+export default routes;
